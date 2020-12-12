374b4b3f7d0f8c32691fa9497a00304575cc9481
Yoel Peter Chandra <yoelpc4@gmail.com>
2020-12-13 01:45:28 +0700

    CHANGE - update service provider

--- src/SailPresetServiceProvider.php
+++ src/SailPresetServiceProvider.php
@@ -18,19 +18,15 @@ class SailPresetServiceProvider extends ServiceProvider implements DeferrablePro
         if ($this->app->runningInConsole()) {
             Artisan::command('sail:publish-preset', function () {
                 if ($this->confirm('Do you want to replace the current runtimes & docker-compose.yml with preset?', false)) {
-                    $this->call('vendor:publish', ['--tag' => 'sail-preset']);
+                    copy(__DIR__.'/../stubs/docker-compose.yml', base_path('docker-compose.yml'));
+                    copy(__DIR__.'/../runtimes/7.4/Dockerfile', base_path('docker/7.4/Dockerfile'));
+                    copy(__DIR__.'/../runtimes/7.4/php.ini', base_path('docker/7.4/php.ini'));
+                    copy(__DIR__.'/../runtimes/8.0/Dockerfile', base_path('docker/8.0/Dockerfile'));
+                    copy(__DIR__.'/../runtimes/8.0/php.ini', base_path('docker/8.0/php.ini'));
 
                     $this->info('Successfully publishing laravel sail preset, please run "sail build --no-cache" to re-building docker image.');
                 }
             })->purpose('Publish the Laravel Sail Docker preset files.');
-
-            $this->publishes([
-                __DIR__.'/../stubs/docker-compose.yml' => base_path('docker-compose.yml'),
-                __DIR__.'/../runtimes/7.4/Dockerfile'  => base_path('docker/7.4/Dockerfile'),
-                __DIR__.'/../runtimes/7.4/php.ini'     => base_path('docker/7.4/php.ini'),
-                __DIR__.'/../runtimes/8.0/Dockerfile'  => base_path('docker/8.0/Dockerfile'),
-                __DIR__.'/../runtimes/8.0/php.ini'     => base_path('docker/8.0/php.ini'),
-            ], 'sail-preset');
         }
     }
 
